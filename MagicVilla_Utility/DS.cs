48ed60930b7932f3fb6379790250aa8db33421e6
rana-621 <ranaebrahim621@gmail.com>
2025-03-08 08:02:05 +0200

    Web Project DTO's and Api Models

---
+++ MagicVilla_Utility/DS.cs
@@ -0,0 +1,13 @@
+﻿namespace MagicVilla_Utility
+{
+    public static class DS
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
---
+++ MagicVilla_Web/Models/APIRequest.cs
@@ -0,0 +1,9 @@
+﻿using static MagicVilla_Utility.DS;
+
+namespace MagicVilla_Web.Models
+{
+    public class APIRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+    }
+}
