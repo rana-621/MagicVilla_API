b4fee4fc852ca4bff601e2346a3bf4ee8e0131ad
rana-621 <ranaebrahim621@gmail.com>
2025-03-08 22:24:48 +0200

    Adding Villa Service

---
+++ MagicVilla_Web/Services/VillaService.cs
@@ -0,0 +1,66 @@
+﻿using MagicVilla_Utility;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+
+namespace MagicVilla_Web.Services
+{
+    public class VillaService : BaseService, IVillaService
+    {
+        public readonly IHttpClientFactory _httpClient;
+        public string villaUrl;
+        public VillaService(IHttpClientFactory httpClient, IConfiguration configuration) : base(httpClient)
+        {
+            _httpClient = httpClient;
+            villaUrl = configuration.GetValue<string>("ServiceUrls: VillaAPI");
+        }
+
+        public async Task<T> CreateAsync<T>(VillaCreateDTO dto)
+        {
+            return await SendAsync<T>(new APIRequest
+            {
+                ApiType = SD.ApiType.POST,
+                Url = villaUrl + "/api/villaApi",
+                Data = dto
+            });
+        }
+
+        public Task<T> DeleteAsync<T>(int id)
+        {
+            return SendAsync<T>(new APIRequest
+            {
+                ApiType = SD.ApiType.DELETE,
+                Url = villaUrl + "/api/villaApi/" + id,
+
+            });
+        }
+
+        public Task<T> GetAllAsync<T>()
+        {
+            return SendAsync<T>(new APIRequest
+            {
+                ApiType = SD.ApiType.GET,
+                Url = villaUrl + "/api/villaApi",
+            });
+        }
+
+        public Task<T> GetAsync<T>(int id)
+        {
+            return SendAsync<T>(new APIRequest
+            {
+                ApiType = SD.ApiType.GET,
+                Url = villaUrl + "/api/villaApi/" + id,
+            });
+        }
+
+        public Task<T> UpdateAsync<T>(VillaUpdateDTO dto)
+        {
+            return SendAsync<T>(new APIRequest
+            {
+                ApiType = SD.ApiType.PUT,
+                Url = villaUrl + "/api/villaApi" + dto.Id,  /// if we do not need the change the id when update the villa 
+                Data = dto
+            });
+        }
+    }
+}
