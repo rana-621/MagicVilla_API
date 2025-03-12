af4e26354b179453848b0ab339c511e257de69e9
rana-621 <ranaebrahim621@gmail.com>
2025-03-12 17:33:47 +0200

    Create Villa UI

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -8,28 +8,47 @@ using Newtonsoft.Json;
 namespace MagicVilla_Web.Controllers
 {
 
-	public class VillaController : Controller
-	{
-		public readonly IVillaService _villaService;
-		public readonly IMapper _mapper;
+    public class VillaController : Controller
+    {
+        public readonly IVillaService _villaService;
+        public readonly IMapper _mapper;
 
-		public VillaController(IVillaService villaService, IMapper mapper)
-		{
-			_villaService = villaService;
-			_mapper = mapper;
-		}
+        public VillaController(IVillaService villaService, IMapper mapper)
+        {
+            _villaService = villaService;
+            _mapper = mapper;
+        }
 
-		public async Task<IActionResult> IndexVilla()
-		{
-			List<VillaDTO> list = new();
-			var response = await _villaService.GetAllAsync<APIResponse>();
-			if (response != null && response.IsSuccess)
-			{
-				list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+        public async Task<IActionResult> IndexVilla()
+        {
+            List<VillaDTO> list = new();
+            var response = await _villaService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
 
-			}
+            }
 
-			return View(list);
-		}
-	}
+            return View(list);
+        }
+        public async Task<ActionResult> CreateVilla()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateVilla(VillaCreateDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _villaService.CreateAsync<APIResponse>(model);
+                if (response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+            }
+            return View(model);
+        }
+    }
 }
