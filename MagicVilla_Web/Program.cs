63326e26356164a0967773b94ce752aef574b4f0
rana-621 <ranaebrahim621@gmail.com>
2025-03-18 23:00:27 +0200

    Add VillaNumberService

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -3,41 +3,44 @@ using MagicVilla_Web.Services.IServices;
 
 namespace MagicVilla_Web
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddAutoMapper(typeof(MapppingConfig));
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
+			builder.Services.AddAutoMapper(typeof(MapppingConfig));
 
-            builder.Services.AddHttpClient<IVillaService, VillaService>();
-            builder.Services.AddScoped<IVillaService, VillaService>();
+			builder.Services.AddHttpClient<IVillaService, VillaService>();
+			builder.Services.AddScoped<IVillaService, VillaService>();
 
-            var app = builder.Build();
+			builder.Services.AddHttpClient<IVillaNumberService, VillaNumberService>();
+			builder.Services.AddScoped<IVillaNumberService, VillaNumberService>();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+			var app = builder.Build();
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
-            app.UseRouting();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
 
-            app.UseAuthorization();
+			app.UseRouting();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+			app.UseAuthorization();
 
-            app.Run();
-        }
-    }
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+
+			app.Run();
+		}
+	}
 }
---
+++ MagicVilla_Web/Services/VillaNumberService.cs
@@ -0,0 +1,67 @@
+﻿using MagicVilla_Utility;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+
+namespace MagicVilla_Web.Services
+{
+	public class VillaNumberService : BaseService, IVillaNumberService
+	{
+		public readonly IHttpClientFactory _httpClient;
+		public string villaNumberUrl;
+
+		public VillaNumberService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory)
+		{
+			_httpClient = clientFactory;
+			villaNumberUrl = configuration.GetValue<string>("ServiceUrls:VillaNumberAPI");
+		}
+
+		public async Task<T> CreateAsync<T>(VillaNumberCreateDto dto)
+		{
+			return await SendAsync<T>(new APIRequest
+			{
+				ApiType = SD.ApiType.POST,
+				Url = villaNumberUrl + "/api/VillaNumberAPI",
+				Data = dto
+
+			});
+		}
+
+		public async Task<T> DeleteAsync<T>(int id)
+		{
+			return await SendAsync<T>(new APIRequest
+			{
+				ApiType = SD.ApiType.DELETE,
+				Url = villaNumberUrl + "/api/VillaNumberAPI/" + id
+			});
+		}
+
+		public async Task<T> GetAllAsync<T>()
+		{
+			return await SendAsync<T>(new APIRequest
+			{
+				ApiType = SD.ApiType.GET,
+				Url = villaNumberUrl + "/api/VillaNumberAPI"
+			});
+		}
+
+		public async Task<T> GetAsync<T>(int id)
+		{
+			return await SendAsync<T>(new APIRequest
+			{
+				ApiType = SD.ApiType.GET,
+				Url = villaNumberUrl + "/api/VillaNumberAPI/" + id
+			});
+		}
+
+		public async Task<T> UpdateAsync<T>(VillaNumberUpdateDto dto)
+		{
+			return await SendAsync<T>(new APIRequest
+			{
+				ApiType = SD.ApiType.PUT,
+				Url = villaNumberUrl + "/api/VillaNumberAPI",
+				Data = dto
+			});
+		}
+	}
+}
