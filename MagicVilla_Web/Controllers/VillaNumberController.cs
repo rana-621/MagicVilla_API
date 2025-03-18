d0493cf4ac91576bd9dda1f16e14fc6f4d8c1187
rana-621 <ranaebrahim621@gmail.com>
2025-03-18 23:37:18 +0200

    Add VillaNumberController

---
+++ MagicVilla_Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_Web.Controllers
+{
+	public class VillaNumberController : Controller
+	{
+		public readonly IVillaNumberService _villaNumberService;
+		public readonly IMapper _mapper;
+
+		public VillaNumberController(IVillaNumberService villaNumberService)
+		{
+			_villaNumberService = villaNumberService;
+		}
+
+	}
+}
