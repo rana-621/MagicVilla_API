5f4f89df48e2e1a78f8667aa4aef61166836adbf
rana-621 <ranaebrahim621@gmail.com>
2025-04-12 11:08:11 +0200

    Implement User Repository - Login Part1

--- MagicVilla_VillaAPI/Repository/UserRepository.cs
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -8,10 +8,11 @@ namespace MagicVilla_VillaAPI.Repository
     public class UserRepository : IUserRepository
     {
         private readonly ApplicationDbContext _db;
-
-        public UserRepository(ApplicationDbContext db)
+        private string secretKey;
+        public UserRepository(ApplicationDbContext db, IConfiguration configuration)
         {
             _db = db;
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret");
         }
 
         public bool isUniqueUser(string username)
@@ -26,7 +27,15 @@ namespace MagicVilla_VillaAPI.Repository
 
         public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
         {
-            throw new NotImplementedException();
+            var user = _db.LocalUsers
+                .FirstOrDefault(u => u.UserName.ToLower() == loginRequestDTO.UserName.ToLower()
+                && u.Password == loginRequestDTO.Password);
+            if (user == null)
+            {
+                return null;
+            }
+
+            //if user was found generate JWT token
         }
 
         public async Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
